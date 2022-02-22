9f23b2bc0bf33fe9ca887afb937ee305ee94f453
dirtyclean <33824512+dirtyclean@users.noreply.github.com>
2022-02-22 17:33:48 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+  <div id="app" style="width: 100%; height: 100%; background: grey; position: relative;">
+      <earth-comp></earth-comp>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import EarthComp from './components/EarthComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    EarthComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
